f38073fe783c48f3c9732387bb65635ece8eb11f
strilchuk_agus <agus.strilchuk@gmail.com>
2021-12-15 20:24:36 -0300

    Agregado de js

---
+++ producto.js
@@ -0,0 +1,10 @@
+class Producto {
+    constructor(nombre, precio, img) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+        this.cant = 1;
+    }
+}
+
+let productos = []
